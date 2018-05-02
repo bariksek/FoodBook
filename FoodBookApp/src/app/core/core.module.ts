e2b84103387a143357781e321b60289ee785264b
SiRazoR <33030760+SiRazoR@users.noreply.github.com>
2018-05-02 15:58:30 +0200

    Reorganization of all components and minor bug fixes

---
+++ FoodBookApp/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+
+/* Tutaj implementowana bedzie obsluga firebase oraz logowania */
+
+/* import { AuthService } from './auth.service';
+import { NotifyService } from './notify.service';
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore'; */
+
+@NgModule({
+  imports: [
+    /* AngularFireAuthModule,
+    AngularFirestoreModule, */
+  ],
+  providers: []
+  /* providers: [AuthService, NotifyService], */
+})
+export class CoreModule { }
