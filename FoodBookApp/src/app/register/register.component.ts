56ca7231bf8c7db6d7a632a7f1ba695f021d86d5
JakubPiotrowsk1 <jakubpiotrowski1@hotmail.com>
2018-05-06 15:28:33 +0200

    Added basic auth with registration

--- FoodBookApp/src/app/register/register.component.ts
+++ FoodBookApp/src/app/register/register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public email: string;
+  public password: string;
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) {}
 
   ngOnInit() {
+  }
+
+  onSubmitAddUser() {
+    this.authService.registerUser(this.email, this.password)
+    .then((res) => {
+      console.log('Registered');
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
 }
