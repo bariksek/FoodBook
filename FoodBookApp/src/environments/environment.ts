2efd2e3bb8565dccad99d4d92d1ee3ca2d3485a8
JakubPiotrowsk1 <jakubpiotrowski1@hotmail.com>
2018-05-06 12:55:56 +0200

    Added firebase configuration

--- FoodBookApp/src/environments/environment.ts
+++ FoodBookApp/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCqMbUXZPp4Bi8b9I_lZwa51esdVEPYvpU",
+    authDomain: "foodbook-4b328.firebaseapp.com",
+    databaseURL: "https://foodbook-4b328.firebaseio.com",
+    projectId: "foodbook-4b328",
+    storageBucket: "",
+    messagingSenderId: "119712383545"
+  }
 };
